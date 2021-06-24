a71a4056f0f3d017b75d4b98b27141307563a42d
ghigbie <gehigbie@gmail.com>
2021-06-24 09:19:56 -0400

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, fuga ex quibusdam sed consequatur at quae enim, tempora provident sint est mollitia reiciendis non exercitationem dicta sequi eaque ut quaerat?</p>
+        </div>
+    )
+}
+
+export default About
