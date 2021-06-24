86264e8486e15b21949da737fe273f75448bfbbb
ghigbie <gehigbie@gmail.com>
2021-06-24 14:32:34 -0400

    Add date to footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,11 @@ import Link from 'next/link';
 import styles from '@/styles/Footer.module.css';
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+
     return (
         <footer className={styles.footer}>
-            <p>Copyright &copy; DJ Events 2021</p>
+            <p>Copyright &copy; DJ Events {year}</p>
             <p>
                 <Link href='/about'>About This Project</Link>
             </p>
